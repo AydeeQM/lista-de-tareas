507ed6d76e309ace99f9489484de6e5e80ed9657
aydee <aydee.qm@gmail.com>
2017-07-26 10:03:50 -0500

    agregando eliminar con el evento addEventListener

--- asset/main.js
+++ asset/main.js
@@ -82,3 +82,11 @@ function addTarea (){
         tarea = document.getElementById('tarea').value = "";
     }  
 }
+
+table1.addEventListener("click", function(event) {
+	if (event.target && event.target.className == "deleteTask") {
+    event.target.parentNode.remove();
+  }
+});
+
+
